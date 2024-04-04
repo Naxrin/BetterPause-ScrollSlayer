db6e1e41e019aa779cb6cf4b4d315ed197f07434
Naxrin <94737247+Naxrin@users.noreply.github.com>
2024-04-04 15:30:33 +0800

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -50,9 +50,9 @@ class $modify(MyLayer, PauseLayer) {
 			auto points = star->getPosition();
 			star->setPosition(CCPoint(points.x, points.y+4));
 
-			auto volfont = menu->getChildByID("volume-settings-time-label");
-			volfont->setAnchorPoint(CCPoint(0.5, 0));
-			volfont->setPosition(CCPoint(winsize.width-160.f, 280));
+			auto volfont = menu->getChildByID("volume-settings-title-label");
+			volfont->setAnchorPoint(CCPoint(0.5, 0.f));
+			volfont->setPosition(CCPoint(winsize.width-145.f, 280.f));
 
 			//two buttons
 			menu->getChildByID("layer-menu-scroll-buttons")->setVisible(false);
@@ -70,14 +70,14 @@ class $modify(MyLayer, PauseLayer) {
 
 			auto resumeButtonImage = cocos2d::CCSprite::createWithSpriteFrameName("GJ_playBtn2_001.png");
 			resumeButtonImage->setScale(0.5f);			
-			auto resumeButton = CCMenuItemSpriteExtra::create(resumeButtonImage, pl, menu_selector(MyLayer::onResume));
+			auto resumeButton = CCMenuItemSpriteExtra::create(resumeButtonImage, pl, menu_selector(PauseLayer::onResume));
 			resumeButton->setPosition(CCPoint(commonx, commony));
 			newmenu->addChild(resumeButton);
 			commony -= 45.f;
 
 			auto replayButtonImage = cocos2d::CCSprite::createWithSpriteFrameName("GJ_replayBtn_001.png");
 			replayButtonImage->setScale(0.5f);			
-			auto replayButton = CCMenuItemSpriteExtra::create(replayButtonImage, pl, menu_selector(MyLayer::onRestart));			
+			auto replayButton = CCMenuItemSpriteExtra::create(replayButtonImage, pl, menu_selector(PauseLayer::onRestart));			
 			replayButton->setPosition(CCPoint(commonx, commony));
 			newmenu->addChild(replayButton);
 			commony -= 40.f;
@@ -85,7 +85,7 @@ class $modify(MyLayer, PauseLayer) {
 			if (PlayLayer::get()->m_level->isPlatformer()) {
 				auto replayFullButtonImage = cocos2d::CCSprite::createWithSpriteFrameName("GJ_replayFullBtn_001.png");
 				replayFullButtonImage->setScale(0.5f);						
-				auto replayFullButton = CCMenuItemSpriteExtra::create(replayFullButtonImage, pl, menu_selector(MyLayer::onRestartFull));
+				auto replayFullButton = CCMenuItemSpriteExtra::create(replayFullButtonImage, pl, menu_selector(PauseLayer::onRestartFull));
 				replayFullButton->setPosition(CCPoint(commonx, commony));
 				newmenu->addChild(replayFullButton);
 				commony -= 40.f;
@@ -93,7 +93,7 @@ class $modify(MyLayer, PauseLayer) {
 
 			auto practiceButtonImage = cocos2d::CCSprite::createWithSpriteFrameName(PlayLayer::get()->m_isPracticeMode ? "GJ_normalBtn_001.png" : "GJ_practiceBtn_001.png");
 			practiceButtonImage->setScale(0.5f);			
-			auto practiceButton = CCMenuItemSpriteExtra::create(practiceButtonImage, pl, (PlayLayer::get()->m_isPracticeMode ? menu_selector(MyLayer::onNormalMode) : menu_selector(MyLayer::onPracticeMode)));			
+			auto practiceButton = CCMenuItemSpriteExtra::create(practiceButtonImage, pl, (PlayLayer::get()->m_isPracticeMode ? menu_selector(PauseLayer::onNormalMode) : menu_selector(PauseLayer::onPracticeMode)));			
 			practiceButton->setPosition(CCPoint(commonx, commony));
 			newmenu->addChild(practiceButton);
 			commony -= 40.f;
@@ -102,9 +102,9 @@ class $modify(MyLayer, PauseLayer) {
 			quitButtonImage->setScale(0.5f);				
 			cocos2d::SEL_MenuHandler handler;
 			#ifdef GEODE_IS_MACOS
-				handler = menu_selector(MyLayer::onQuit);
+				handler = menu_selector(PauseLayer::onQuit);
 			#else
-				handler = menu_selector(MyLayer::tryQuit);
+				handler = menu_selector(PauseLayer::tryQuit);
 			#endif
 				auto quitButton = CCMenuItemSpriteExtra::create(quitButtonImage, pl, handler);
 				quitButton->setPosition(CCPoint(commonx, commony));
@@ -114,7 +114,7 @@ class $modify(MyLayer, PauseLayer) {
 			if (PlayLayer::get()->m_level->m_levelType == GJLevelType::Editor || Mod::get()->getSettingValue<bool>("level-edit")) {
 				auto editButtonImage = cocos2d::CCSprite::createWithSpriteFrameName("GJ_editBtn_001.png");
 				editButtonImage->setScale(0.38);		
-				auto editButton = CCMenuItemSpriteExtra::create(editButtonImage, pl, menu_selector(MyLayer::onEdit));
+				auto editButton = CCMenuItemSpriteExtra::create(editButtonImage, pl, menu_selector(PauseLayer::onEdit));
 				editButton->setPosition(CCPoint(commonx, commony));
 				newmenu->addChild(editButton);
 				commony -= 40.f;
@@ -122,7 +122,7 @@ class $modify(MyLayer, PauseLayer) {
 			if (Mod::get()->getSettingValue<bool>("show-comments")){
 				auto showCommentsImage = cocos2d::CCSprite::createWithSpriteFrameName("GJ_chatBtn_001.png");
 				showCommentsImage->setScale(0.7f);
-				auto showCommentsButton = CCMenuItemSpriteExtra::create(showCommentsImage, pl, menu_selector(MyLayer::onInfoAlert));			
+				auto showCommentsButton = CCMenuItemSpriteExtra::create(showCommentsImage, pl, menu_selector(MyLayer::onLevelInfoLayer));			
 				showCommentsButton->setPosition(CCPoint(commonx, 35.f));			
 				newmenu->addChild(showCommentsButton);
 			}
